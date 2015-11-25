daa6355237665d1b29f678ef5c6d6bd989f98723
mesargent <markesargent@gmail.com>
2015-11-24 20:11:08 -0800

    bug fixes

--- django_project/classifierapi/views.py
+++ django_project/classifierapi/views.py
@@ -94,9 +94,7 @@ class LogisticRegression(APIView):
         request.session['accuracy'] = accuracy
         confusion_matrix = metrics.confusion_matrix(labels_test, pred)
 
-        confusion_matrix = metrics.confusion_matrix([1, 1, 1, 1, 0, 1], 
-
-                                                    [1, 1, 0, 1, 1, 0])
+        confusion_matrix = metrics.confusion_matrix([labels_test, pred)
         print "confusion matrix: ", confusion_matrix
         request.session['confusion_matrix'] = confusion_matrix.tolist()
         f1 = metrics.f1_score(labels_test, pred)
